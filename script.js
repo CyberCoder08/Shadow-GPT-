1c5062f2549f2b208c81f56f75749dede58fc77c
CyberCoder08 <yashchoubey31@gmail.com>
2025-11-29 13:46:28 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const res = await fetch("/api/generate", {
-  method: "POST",
-  headers: { "Content-Type": "application/json" },
-  body: JSON.stringify({ prompt: userInput })
-});
-const data = await res.json();
